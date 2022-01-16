4295bc39c732a1eb5ed3526c5ee4c58d8ef116bc
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2022-01-16 22:06:40 +0300

    created statics page:

--- includes/home.php
+++ includes/home.php
@@ -1,105 +1,169 @@
-<h1 class="my-3">My Wordpress Statistics</h1>
-<div class="row container p-0">
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="card bg-info">
-            <div class="inner">
-                <h3>150</h3>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Stats</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+    />
+  </head>
 
-                <p>Total Posts</p>
+  <body>
+    <div class="flex flex-col gap-8 px-2 py-4 ml-32">
+      <div class="text-3xl font-medium">My Wordpress Analytics</div>
+      <div id="overview" class="">
+        <div class="grid grid-cols-4 grid-rows-2 gap-4">
+          <div class="row-span-2">
+            <div
+              class="flex flex-col content-center items-center bg-sky-700 w-full h-full rounded-xl gap-2 py-5"
+            >
+              <div
+                class="bg-sky-600 rounded-full px-4 py-2 shadow-lg text-center"
+              >
+                <i class="fas fa-thumbtack text-4xl text-white"></i>
+              </div>
+              <h1 class="text-6xl text-white font-bold">5</h1>
+              <p class="text-white">Today Posts</p>
             </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Posts
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-sky-200 rounded-full px-4 py-2 shadow text-center"
+                >
+                  <i class="fas fa-thumbtack text-2xl text-sky-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
             </div>
+            <!-- card end here -->
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Comments
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-indigo-300 rounded-full px-3 py-2 shadow text-center"
+                >
+                  <i class="fas fa-comments text-2xl text-indigo-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
+            </div>
+            <!-- card end here -->
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Pages
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-red-300 rounded-full px-3 py-2 shadow text-center"
+                >
+                  <i class="fas fa-copy text-2xl text-red-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
+            </div>
+            <!-- card end here -->
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Users
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-orange-300 rounded-full p-2 shadow text-center"
+                >
+                  <i class="fas fa-users text-2xl text-orange-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
+            </div>
+            <!-- card end here -->
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Media
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-teal-300 rounded-full px-3 py-2 shadow text-center"
+                >
+                  <i class="fa fa-images text-2xl text-teal-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
+            </div>
+            <!-- card end here -->
+          </div>
+          <div>
+            <!-- card start here -->
+            <div
+              class="flex flex-col gap-2 bg-white drop-shadow hover:drop-shadow-lg rounded-xl px-5 py-4"
+            >
+              <h4 class="font-semibold text-gray-500 text-xs uppercase">
+                Total Tags and Catagories
+              </h4>
+              <div class="flex flex-row gap-3">
+                <div
+                  class="bg-pink-300 rounded-full p-2 shadow text-center"
+                >
+                  <i class="fas fa-tags text-2xl text-pink-600"></i>
+                </div>
+                <h1 class="text-4xl font-semibold">200</h1>
+              </div>
+            </div>
+            <!-- card end here -->
+          </div>
+          <!-- <div class="row-span-3">
+                <div class="bg-black w-full h-full rounded-xl">1</div>
+            </div>
+            <div class="col-span-2 ...">
+                <div class="bg-red-700 w-full h-full rounded-xl">2</div>
+            </div>
+            <div class="row-span-2 col-span-2 ...">
+                <div class="bg-blue-700 w-full h-full rounded-xl">3</div>
+            </div> -->
         </div>
+      </div>
     </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="card bg-success">
-            <div class="inner">
-                <h3>53</h3>
-
-                <p>Total Comments</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-            </div>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="card bg-warning">
-            <div class="inner">
-                <h3>44</h3>
-
-                <p>Total Media</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-person-add"></i>
-            </div>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="card bg-danger">
-            <div class="inner">
-                <h3>65</h3>
-
-                <p>Total Visitors</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-            </div>
-        </div>
-    </div>
-    <!-- ./col -->
-</div>
-
-
-<div class="px-2 pt-4">
-    <h3 class="py-2">Graphical Analytics</h3>
-    <select>
-        <option value="today">Today</option>
-    </select>
-    <canvas id="myChart" height="120"></canvas>
-    <script>
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['views', 'comments', 'posts', 'media', 'users'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [3, 2, 3, 5, 2],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
-</div>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+  </body>
+</html>
