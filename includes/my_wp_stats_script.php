cc8bd65ff03a4bd451ae123688604a90341b08b1
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2022-01-18 03:45:33 +0300

    moved plugin script to includes

---
+++ includes/my_wp_stats_script.php
@@ -0,0 +1,34 @@
+<?php
+// register jquery and style on initialization
+add_action('init', 'register_script');
+function register_script() {
+    wp_register_style("tailwind",  plugins_url().'/my-wp-stats/assets/css/tailwind.min.css');
+    wp_register_style("dataTable", plugins_url().'/my-wp-stats/assets/css/vanilla-dataTables.min.css');
+    wp_register_style( 'fontAwesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css' );
+    wp_register_script("tailwindjs", plugins_url().'/my-wp-stats/assets/js/tailwind.js');
+    wp_register_script("jquery", plugins_url().'/my-wp-stats/assets/js/jquery-3.6.0.min.js');
+    wp_register_script("apexChart", plugins_url().'/my-wp-stats/assets/js/apexcharts.js');
+    wp_register_script("dataTablejs", plugins_url().'/my-wp-stats/assets/js/simple-datatables.js');
+   
+}
+
+add_action("admin_menu", "addMenu");
+function addMenu()
+{
+    
+    wp_enqueue_style('tailwind');
+    wp_enqueue_style('fontAwesome');
+    wp_enqueue_style('dataTable');
+
+    wp_enqueue_script('tailwindjs');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('apexChart');
+    wp_enqueue_script('dataTablejs');
+    add_menu_page("My WP Stats", "My WP Stats", 4, "my-wp-stats", "launchWpStats" );
+}
+
+function launchWpStats()
+{
+    require("template/home.php");
+}
+?>
--- my-wp-stats.php
+++ my-wp-stats.php
@@ -7,36 +7,22 @@
  * Author: Michael Belete
  * Author URI: http://www.mywebsite.com
  */
-
-// register jquery and style on initialization
-add_action('init', 'register_script');
-function register_script() {
-    wp_register_style("tailwind",  plugins_url().'/my-wp-stats/assets/css/tailwind.min.css');
-    wp_register_style("dataTable", plugins_url().'/my-wp-stats/assets/css/vanilla-dataTables.min.css');
-    wp_register_style( 'fontAwesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css' );
-    wp_register_script("tailwindjs", plugins_url().'/my-wp-stats/assets/js/tailwind.js');
-    wp_register_script("jquery", plugins_url().'/my-wp-stats/assets/js/jquery-3.6.0.min.js');
-    wp_register_script("apexChart", plugins_url().'/my-wp-stats/assets/js/apexcharts.js');
-    wp_register_script("dataTablejs", plugins_url().'/my-wp-stats/assets/js/simple-datatables.js');
-   
+// Exit if accessed directly
+if(!defined('ABSPATH')){
+    exit;
 }
 
-add_action("admin_menu", "addMenu");
-function addMenu()
-{
-    
-    wp_enqueue_style('tailwind');
-    wp_enqueue_style('fontAwesome');
-    wp_enqueue_style('dataTable');
+// Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/my_wp_stats_script.php');
 
-    wp_enqueue_script('tailwindjs');
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('apexChart');
-    wp_enqueue_script('dataTablejs');
-    add_menu_page("My WP Stats", "My WP Stats", 4, "my-wp-stats", "launchWpStats" );
-}
+// // Load Class
+// require_once(plugin_dir_path(__FILE__).'/includes/youtubesubs-class.php');
 
-function launchWpStats()
-{
-    require("includes/home.php");
-}
+// // Register Widget
+// function register_youtubesubs(){
+//   register_widget('Youtube_Subs_Widget');
+// }
+
+// // Hook in function
+// add_action('widgets_init', 'register_youtubesubs');
+
